f792d91a4c0c685048617e4ffdf7ebc643a99186
krups210 <krupalgithub210@gmail.com>
2025-06-29 04:51:18 -0700

    classes code 1 to 17 files

---
+++ Java codes lecture wise/P007_Patterns.java
@@ -0,0 +1,12 @@
+package core;
+
+public class P007_Patterns {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 4; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
